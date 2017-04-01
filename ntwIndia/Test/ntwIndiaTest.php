78bf50d3fd3508377b8d7b32e0df881cb73bc718
Swashata Ghosh <swashata4u@gmail.com>
2017-04-01 12:22:03 +0530

    Add more test cases
    We add zero output and testcase for num_to_wd_small

--- ntwIndia/Test/ntwIndiaTest.php
+++ ntwIndia/Test/ntwIndiaTest.php
@@ -72,4 +72,47 @@ class NTW_India_Test extends \PHPUnit\Framework\TestCase {
 			$this->assertEquals( $wd, $this->ntw->num_to_word( $num ) );
 		}
 	}
+
+	public function test_zero_value() {
+		$num = 0;
+		$word = 'Zero';
+		$this->assertEquals( $word, $this->ntw->num_to_word( $num ) );
+	}
+
+	public function test_num_to_wd_small() {
+		$num_to_wd = array(
+			'0'  => 'Zero',
+			'1'  => 'One',
+			'2'  => 'Two',
+			'3'  => 'Three',
+			'4'  => 'Four',
+			'5'  => 'Five',
+			'6'  => 'Six',
+			'7'  => 'Seven',
+			'8'  => 'Eight',
+			'9'  => 'Nine',
+			'10' => 'Ten',
+			'11' => 'Eleven',
+			'12' => 'Twelve',
+			'13' => 'Thirteen',
+			'14' => 'Fourteen',
+			'15' => 'Fifteen',
+			'16' => 'Sixteen',
+			'17' => 'Seventeen',
+			'18' => 'Eighteen',
+			'19' => 'Nineteen',
+			'20' => 'Twenty',
+			'30' => 'Thirty',
+			'40' => 'Forty',
+			'50' => 'Fifty',
+			'60' => 'Sixty',
+			'70' => 'Seventy',
+			'80' => 'Eighty',
+			'90' => 'Ninety',
+		);
+
+		foreach ( $num_to_wd as $num => $wd ) {
+			$this->assertEquals( $wd, $this->ntw->num_to_wd_small( $num ) );
+		}
+	}
 }
