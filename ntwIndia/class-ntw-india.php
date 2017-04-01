c80343a52233d7b144c80cd18066915c990888de
Swashata Ghosh <swashata4u@gmail.com>
2017-04-01 09:43:13 +0530

    Add composer and testing framework
    We don't need the vendor since it is managed by composer

---
+++ ntwIndia/class-ntw-india.php
@@ -0,0 +1,45 @@
+<?php
+namespace ntwIndia;
+/**
+ * Number to Words in Indian Version
+ *
+ * PHP Library
+ *
+ * @author Swashata Ghosh <swashata@iptms.co>
+ * @license GPLv3
+ */
+class NTW_India {
+	public $hundred = 'Hundred';
+	public $thousand = 'Thousand';
+	public $lakh = 'Lakh';
+	public $crore = 'Crore';
+	public $num_to_bd = array(
+		'1'  => 'One',
+		'2'  => 'Two',
+		'3'  => 'Three',
+		'4'  => 'Four',
+		'5'  => 'Five',
+		'6'  => 'Six',
+		'7'  => 'Seven',
+		'8'  => 'Eight',
+		'9'  => 'None',
+		'10' => 'Ten',
+		'11' => 'Eleven',
+		'12' => 'Twelve',
+		'13' => 'Thirteen',
+		'14' => 'Fourteen',
+		'15' => 'Fifteen',
+		'16' => 'Sixteen',
+		'17' => 'Seventeen',
+		'18' => 'Eighteen',
+		'19' => 'Nineteen',
+		'20' => 'Twenty',
+		'30' => 'Thirty',
+		'40' => 'Forty',
+		'50' => 'Fifty',
+		'60' => 'Sixty',
+		'70' => 'Seventy',
+		'80' => 'Eighty',
+		'90' => 'Ninety',
+	);
+}
